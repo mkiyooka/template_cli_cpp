7ec75665f7a22ee95720d676ee23a2bf664e8ca7
Masahiro KIYOOKA <kiyookamasa@gmail.com>
2025-10-10 23:25:35 +0900

    chore: FetchContent呼び出し最適化

--- cmake/dependencies-app.cmake
+++ cmake/dependencies-app.cmake
@@ -1,36 +1,36 @@
 # メインアプリケーション用のサードパーティライブラリ定義
 #
 # 使用例:
-#   declare_fetchcontent_with_local(fmt third_party/fmt-11.2.0
-#       GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-#       GIT_TAG 11.2.0
+#   add_external_package(fmt third_party/fmt-12.0.0
+#       URL https://github.com/fmtlib/fmt/archive/refs/tags/12.0.0.tar.gz
+#       URL_HASH SHA256=...
 #   )
 #   FetchContent_MakeAvailable(fmt)
 
 # CLI11 - Command line parser
-declare_fetchcontent_with_local(CLI11 third_party/CLI11-2.5.0
-    GIT_REPOSITORY https://github.com/CLIUtils/CLI11.git
-    GIT_TAG v2.5.0
+add_external_package(CLI11 third_party/CLI11-2.5.0
+    URL https://github.com/CLIUtils/CLI11/archive/refs/tags/v2.5.0.tar.gz
+    URL_HASH SHA256=17e02b4cddc2fa348e5dbdbb582c59a3486fa2b2433e70a0c3bacb871334fd55
 )
 FetchContent_MakeAvailable(CLI11)
 
 # fmt - Formatting library
-declare_fetchcontent_with_local(fmt third_party/fmt-11.2.0
-    GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-    GIT_TAG 11.2.0
+add_external_package(fmt third_party/fmt-12.0.0
+    URL https://github.com/fmtlib/fmt/archive/refs/tags/12.0.0.tar.gz
+    URL_HASH SHA256=aa3e8fbb6a0066c03454434add1f1fc23299e85758ceec0d7d2d974431481e40
 )
 FetchContent_MakeAvailable(fmt)
 
 # tomlplusplus - TOML configuration library
-declare_fetchcontent_with_local(tomlplusplus third_party/tomlplusplus-3.4.0
-    GIT_REPOSITORY https://github.com/marzer/tomlplusplus.git
-    GIT_TAG v3.4.0
+add_external_package(tomlplusplus third_party/tomlplusplus-3.4.0
+    URL https://github.com/marzer/tomlplusplus/archive/refs/tags/v3.4.0.tar.gz
+    URL_HASH SHA256=8517f65938a4faae9ccf8ebb36631a38c1cadfb5efa85d9a72e15b9e97d25155
 )
 FetchContent_MakeAvailable(tomlplusplus)
 
 # argparse libraries (header-only)
 # morrisfranken/argparse - use header-only approach
-declare_fetchcontent_with_local(argparse_morris third_party/argparse_morris-master
+add_external_package(argparse_morris third_party/argparse_morris-master
     GIT_REPOSITORY https://github.com/morrisfranken/argparse.git
     GIT_TAG master
 )
